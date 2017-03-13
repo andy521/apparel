51b6a96c3a38cdf1ee8b989185776b7d193c4afd
王英豪 <wangyh@e-sinhai.com>
2017-03-13 23:44:43 +0800

    tabLayout 重写点击 自定义样式

--- app/src/main/java/com/yhao/view/HomeActivity.java
+++ app/src/main/java/com/yhao/view/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.yhao.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.yhao.view.adapter.ViewPagerAdapter;
 import com.yhao.view.widgt.MyViewPager;
@@ -13,6 +16,7 @@ public class HomeActivity extends AppCompatActivity {
     private MyViewPager mViewPager;
     private TabLayout mTabLayout;
 
+    private static final int[] pImageResId = new int[]{R.mipmap.home_p, R.mipmap.type_p, R.mipmap.find_p, R.mipmap.car_p, R.mipmap.personal_p};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,62 +29,21 @@ public class HomeActivity extends AppCompatActivity {
     private void initView() {
         mViewPager = (MyViewPager) findViewById(R.id.viewPager);
         mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        mViewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(viewPagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
-        View tabView = LayoutInflater.from(this).inflate(R.layout.tablayout_item, null);
+        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = mTabLayout.getTabAt(i);
+            View customView = viewPagerAdapter.getCustomView(this, i);
+            tab.setCustomView(customView);
+            View tabView = (View) tab.getCustomView().getParent();
+            tabView.setBackgroundColor(Color.WHITE);
+            int finalI = i;
+            tabView.setOnClickListener(v -> {
+                ImageView image = (ImageView) customView.findViewById(R.id.icon);
+                image.setImageResource(pImageResId[finalI]);
+            });
+        }
 
-        mTabLayout.getTabAt(0).setIcon(R.mipmap.home);
-        mTabLayout.getTabAt(1).setIcon(R.mipmap.type);
-        mTabLayout.getTabAt(2).setIcon(R.mipmap.find);
-        mTabLayout.getTabAt(3).setIcon(R.mipmap.car);
-        mTabLayout.getTabAt(4).setIcon(R.mipmap.personal);
-//        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                switch (tab.getPosition()) {
-//                    case 0:
-//                        tab.setIcon(R.mipmap.home_p);
-//                        break;
-//                    case 1:
-//                        tab.setIcon(R.mipmap.type_p);
-//                        break;
-//                    case 2:
-//                        tab.setIcon(R.mipmap.find_p);
-//                        break;
-//                    case 3:
-//                        tab.setIcon(R.mipmap.car_p);
-//                        break;
-//                    case 4:
-//                        tab.setIcon(R.mipmap.personal_p);
-//                        break;
-//                }
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//                switch (tab.getPosition()) {
-//                    case 0:
-//                        tab.setIcon(R.mipmap.home);
-//                        break;
-//                    case 1:
-//                        tab.setIcon(R.mipmap.type);
-//                        break;
-//                    case 2:
-//                        tab.setIcon(R.mipmap.find);
-//                        break;
-//                    case 3:
-//                        tab.setIcon(R.mipmap.car);
-//                        break;
-//                    case 4:
-//                        tab.setIcon(R.mipmap.personal);
-//                        break;
-//                }
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
     }
 }
--- app/src/main/java/com/yhao/view/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/yhao/view/adapter/ViewPagerAdapter.java
@@ -1,9 +1,15 @@
 package com.yhao.view.adapter;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.yhao.view.R;
 import com.yhao.view.fragment.CarFragment;
 import com.yhao.view.fragment.FindFragment;
 import com.yhao.view.fragment.HomeFragment;
@@ -19,6 +25,8 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     private static final int pagerNum = 5;
     private static final String[] pagerTitle = new String[]{"首页", "分类", "发现", "购物车", "我的"};
+    private static final int[] imageResId = new int[]{R.mipmap.home, R.mipmap.type, R.mipmap.find, R.mipmap.car, R.mipmap.personal};
+    private static final int[] pImageResId = new int[]{R.mipmap.home_p, R.mipmap.type_p, R.mipmap.find_p, R.mipmap.car_p, R.mipmap.personal_p};
 
 
     public ViewPagerAdapter(FragmentManager fm) {
@@ -50,13 +58,22 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
         return fragment;
     }
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return pagerTitle[position];
+    public View getCustomView(Context context, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.tablayout_item, null);
+        TextView tv = (TextView) view.findViewById(R.id.text);
+        ImageView img = (ImageView) view.findViewById(R.id.icon);
+        tv.setText(pagerTitle[position]);
+        img.setImageResource(imageResId[position]);
+        return view;
     }
 
     @Override
     public int getCount() {
         return pagerNum;
+    }
+
+
+    public void seleteItem(int finalI) {
+
     }
 }

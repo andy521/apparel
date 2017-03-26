c7c939e6288a482bb00d545a016453fa963c92d9
yhao <yhaowa@outlook.com>
2017-03-26 23:47:07 +0800

    TODO: ConstraintLayout

---
+++ app/src/main/java/com/yhao/view/SWaresActivity.java
@@ -0,0 +1,14 @@
+package com.yhao.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SWaresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swares);
+    }
+}
